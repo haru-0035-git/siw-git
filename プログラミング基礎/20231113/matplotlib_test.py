7ae828c808b37413c59eed39d72718bd8c6e0b0b
moteki <>
2024-02-03 15:35:34 +0900

    z

--- プログラミング基礎/20231113/matplotlib_test.py
+++ プログラミング基礎/20231113/matplotlib_test.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-data = np.loadtxt('fm08_m_1.csv',delimiter=',')
+data = np.loadtxt(f'{os.path.dirname(__file__)}/fm08_m_1.csv',delimiter=',')
 plt.plot(data)
 plt.show()
