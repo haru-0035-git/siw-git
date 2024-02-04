28f66dc3f03eb132e99a17ba6564b06f978678ee
moteki <>
2024-02-04 17:51:36 +0900

    datetime

--- test_後期/プログラミング基礎/20240129復習/date_now.py
+++ test_後期/プログラミング基礎/20240129復習/date_now.py
@@ -2,5 +2,12 @@ import datetime
 
 now = datetime.datetime.now()
 print(now.date())
+print(now.time())
 now_f = datetime.datetime.strftime(now,'%Y年%m月%d日')
 print(now_f)
+
+weekday = list('月火水木金土日')
+
+now_weekday = datetime.date.weekday(now)
+print(weekday)
+print(f'今日は、{weekday[now_weekday]}曜日')
